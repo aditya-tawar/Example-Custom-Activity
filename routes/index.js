32b432f80a3013ba78f0507e4d78ba66daba1539
Aditya Tawar <89097104+aditya-tawar@users.noreply.github.com>
2024-09-03 17:27:40 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Deps
+console.log("in the index file ");
+var activity = require('./activity');
+
+/*
+ * GET home page.
+ */
+exports.index = function(req, res){
+    if( !req.session.token ) {
+        res.render( 'index', {
+            title: 'Unauthenticated',
+            errorMessage: 'This app may only be loaded via Salesforce Marketing Cloud',
+        });
+    } else {
+        res.render( 'index', {
+            title: 'Journey Builder Activity',
+            results: activity.logExecuteData,
+        });
+    }
+};
+
+exports.login = function( req, res ) {
+    console.log( 'req.body: ', req.body );
+    console.log('hello login function called');
+    res.redirect( '/' );
+};
+
+exports.logout = function( req, res ) {
+    req.session.token = '';
+};
