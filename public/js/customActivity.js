f1124e83e681ff48dab88b1777b6ebb8d315f7e3
Aditya Tawar <89097104+aditya-tawar@users.noreply.github.com>
2024-09-09 15:20:38 +0530

    Add files via upload

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -4,7 +4,7 @@ define([
     Postmonger
 ) {
     'use strict';
-
+    console.log("in the custom activity ");
     var connection = new Postmonger.Session();
     var payload = {};
     var lastStepEnabled = false;
@@ -13,22 +13,37 @@ define([
     ];
     var currentStep = steps[0].key;
 
+    console.log("Before windows is ready");
     $(window).ready(onRender);
-
+    console.log("Before initializing");
     connection.on('initActivity', initialize);
+    console.log("Before requesting tokens");
     connection.on('requestedTokens', onGetTokens);
+    console.log("Before requesting endpoints");
     connection.on('requestedEndpoints', onGetEndpoints);
+    console.log("After requesting endpoints");
 
     connection.on('clickedNext', save);
+    console.log("After clicking done");
     //connection.on('clickedBack', onClickedBack);
     //connection.on('gotoStep', onGotoStep);
 
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
+        console.log("Entered onRender function");
         connection.trigger('ready');
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
     }
+
+    var eventDefinitionKey;
+    connection.on("requestedTriggerEventDefinition", function (
+    eventDefinitionModel
+    ) {
+        if (eventDefinitionModel) {
+        eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+        }
+    });
 
   function initialize(data) {
         console.log("Initializing data data: "+ JSON.stringify(data));
@@ -58,9 +73,9 @@ define([
                     $('#authToken').val(val);
                 }
 
-                if (key === 'messagingService') {
-                    $('#messagingService').val(val);
-                }
+                // if (key === 'messagingService') {
+                //     $('#messagingService').val(val);
+                // }
 
                 if (key === 'body') {
                     $('#messageBody').val(val);
@@ -69,6 +84,7 @@ define([
             })
         });
 
+        console.log("Before changing the button to done");
         connection.trigger('updateButton', {
             button: 'next',
             text: 'done',
@@ -84,30 +100,32 @@ define([
     }
 
     function onGetEndpoints (endpoints) {
-        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+         //Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
         console.log("Get End Points function: "+JSON.stringify(endpoints));
     }
 
     function save() {
 
-        var accountSid = $('#accountSID').val();
+        console.log("Entered save function");
+        var accountSid = $('#accountSid').val();
         var authToken = $('#authToken').val();
-        var messagingService = $('#messagingService').val();
+    //    var messagingService = $('#messagingService').val();
         var body = $('#messageBody').val();
-
+        console.log("in the save option "+ body);
+        
+        console.log("Before populating the payload");
         payload['arguments'].execute.inArguments = [{
             "accountSid": accountSid,
             "authToken": authToken,
-            "messagingService": messagingService,
+    //        "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.TwilioV1.TwilioNumber}}" //<----This should map to your data extension name and phone number column
-        }];
-
+            "to": "{{Events."+eventDefinitionKey+".telegramid}}" ,//<----This should map to your data extension name and phone number column
+           
+        }];       
         payload['metaData'].isConfigured = true;
-
         console.log("Payload on SAVE function: "+JSON.stringify(payload));
         connection.trigger('updateActivity', payload);
-
+        console.log("after triggering the update activity");
     }                    
-
+x``
 });
